83f00ff855a3a55e381fe1d4fcac77eb57a8622b
Anticubex <70480087+Anticubex@users.noreply.github.com>
2023-07-10 19:47:19 +0000

    Working

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -5,17 +5,58 @@
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let mut x: usize = 0;
+    let mut y: usize = input.len();
+
+    let asBytes = input.as_bytes();
+
+    for i in 0..input.len() {
+        if asBytes[i] as char != ' ' {
+            break;
+        }
+        x += 1;
+    }
+
+    for i in 1..input.len() + 1 {
+        if asBytes[input.len() - i] as char != ' ' {
+            break;
+        }
+        y -= 1;
+    }
+
+    input[x..y].to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut string = input.to_string();
+    string.push_str(" world!");
+    String::from(string)
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let mut string = "".to_string();
+    let asBytes = input.as_bytes();
+
+    let mut i: usize = 0;
+
+    // const cars: String = "cars".chars().take(4).collect();
+
+    while i <= input.len() - 4 {
+        if &input[i..i + 4] == "cars" {
+            println!("Matched");
+            string.push_str("balloons");
+            i += 4;
+            continue;
+        }
+        string.push(asBytes[i] as char);
+        i += 1;
+    }
+
+    string.push_str(&input[i..input.len()]);
+
+    string
 }
 
 #[cfg(test)]
@@ -37,7 +78,13 @@ mod tests {
 
     #[test]
     fn replace_a_string() {
-        assert_eq!(replace_me("I think cars are cool"), "I think balloons are cool");
-        assert_eq!(replace_me("I love to look at cars"), "I love to look at balloons");
+        assert_eq!(
+            replace_me("I think cars are cool"),
+            "I think balloons are cool"
+        );
+        assert_eq!(
+            replace_me("I love to look at cars"),
+            "I love to look at balloons"
+        );
     }
 }
